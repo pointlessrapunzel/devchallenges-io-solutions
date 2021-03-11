8ba924aac0a1c468ae1ff416ee8b8bd5eb87efcd
pointlessrapunzel <vladace9@gmail.com>
2021-03-11 05:52:54 +0500

    Add Button component
    Add Button stories, styles

---
+++ frontend/react-components/src/components/Button/Button.stories.tsx
@@ -0,0 +1,48 @@
+import Button, { BtnProps } from './Button'
+import { Meta, Story } from '@storybook/react'
+
+export default {
+  component: Button,
+  title: 'Button',
+} as Meta
+
+const Template: Story<BtnProps> = (args) => <Button {...args}>Default</Button>
+
+export const Default = Template.bind({})
+Default.args = {}
+
+export const Outline = Template.bind({})
+Outline.args = {
+  variant: 'outline',
+}
+
+export const Text = Template.bind({})
+Text.args = {
+  variant: 'text',
+}
+
+export const DisabledShadow = Template.bind({})
+DisabledShadow.args = {
+  disableShadow: true,
+  color: 'primary',
+}
+
+export const Disabled = Template.bind({})
+Disabled.args = {
+  disabled: true,
+}
+
+export const Primary = Template.bind({})
+Primary.args = {
+  color: 'primary',
+}
+
+export const Secondary = Template.bind({})
+Secondary.args = {
+  color: 'secondary',
+}
+
+export const Danger = Template.bind({})
+Danger.args = {
+  color: 'danger',
+}
---
+++ frontend/react-components/src/components/Button/Button.tsx
@@ -0,0 +1,39 @@
+import S from './styles/Button.module.scss'
+
+// TODO: Icons, size
+
+const Button: React.FC<BtnProps> = ({
+  children,
+  variant = 'default',
+  disableShadow = false,
+  disabled = false,
+  startIcon,
+  endIcon,
+  size = 'md',
+  color = 'default',
+}) => {
+  let variantClass = S[`btn--${variant}`] || ''
+  let colorClass = S[`btn--${color}`] || ''
+  let shadowClass = disableShadow ? S[`btn--no-shadow`] : ''
+
+  let className = `${S.btn} ${variantClass} ${colorClass} ${shadowClass}`
+
+  const clickHandler = () => console.log('button clicked!')
+  return (
+    <button className={className} disabled={disabled} onClick={clickHandler}>
+      {children}
+    </button>
+  )
+}
+
+export interface BtnProps {
+  variant?: 'default' | 'outline' | 'text'
+  disableShadow?: boolean
+  disabled?: boolean
+  startIcon?: string
+  endIcon?: string
+  size?: 'sm' | 'md' | 'lg'
+  color?: string
+}
+
+export default Button
